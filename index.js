740a82dd8b8d7658a94acb7c6a47013baed43466
Gourie <gouriejagtap@gmail.com>
2022-02-16 18:53:32 +0530

    commit4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var min=0;
-var hr=0;
-var sec=0;
+var min=00;
+var hr=00;
+var sec=00;
 
 var timer=false;
 
@@ -12,7 +12,15 @@ function stop(){
     timer=false;
 }
 function reset(){
+    timer=false;
+    min=0;
+    hr=0;
+    sec=0;
 
+    document.getElementById('hr').innerHTML="00";
+    document.getElementById('min').innerHTML="00";
+    document.getElementById('sec').innerHTML="00";
+    setTimeout('stopwatch()',1000);
 }
 
 function stopwatch(){
@@ -21,10 +29,31 @@ function stopwatch(){
         if(sec==60){
             min=min+1;
             sec=0;
-
+        }if(min==60){
+            hr=hr+1;
+            min=0;
+            sec=0;
         }
-        document.getElementById('hr').innerHTML=hr;
-        document.getElementById('sec').innerHTML=sec;
-        settimeout('stopwatch()',100);
+        var hrstring=hr;
+        var minstring=min;
+        var secstring=sec;
+
+        if (hr<10)
+        {
+            hrstring="0" +hrstring;
+        }
+        if (sec<10)
+        {
+            secstring="0" +secstring;
+        }
+        if (min<10)
+        {
+            minstring="0" +minstring;
+        }
+
+        document.getElementById('hr').innerHTML=hrstring;
+        document.getElementById('min').innerHTML=minstring;
+        document.getElementById('sec').innerHTML=secstring;
+        setTimeout('stopwatch()',1000);
     }
 }
